4fd2d2097623651f6083df716619797ed0763916
anticaniket <baniket141@gmail.com>
2019-12-21 17:25:49 +0530

    admin & user table added

---
+++ database/migrations/2019_12_21_115040_create_admin_users_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdminUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admin_users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('u_id');
+            $table->string('mobile');
+            $table->string('alternate_mobile');
+            $table->string('alternate_email');
+            $table->string('aadhar_card');
+            $table->string('pan_card');
+            $table->string('gender');
+            $table->string('state');
+            $table->string('city');
+            $table->integer('pincode');
+            $table->string('correspondence_address');
+            $table->string('permanent_address');
+            $table->string('photo_path');
+            $table->string('signature_path');
+            $table->timestamps();
+           
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admin_users');
+    }
+}
